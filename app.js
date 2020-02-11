8c9d2696374879586302b1ab3d559f09eb3358b2
DrGooseman <jamesjquinn12@gmail.com>
2020-02-11 20:56:01 +0300

    adds connection to MongoDB

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
-const items = ["test 1", "test 2", "test 3"];
-const workItems = ["test 1", "test 2", "test 3"];
+//const items = ["test 1", "test 2", "test 3"];
+//const workItems = ["test 1", "test 2", "test 3"];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true
+});
+
+const itemSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemSchema);
+
+// const item1 = new Item({ name: "Buy Food" });
+// const item2 = new Item({ name: "Cook Food" });
+// const item3 = new Item({ name: "Eat Food" });
+
+// const defaultItems = [item1, item2, item3];
+
+// Item.insertMany(defaultItems, function(err) {
+//   if (err) console.log(err);
+//   else console.log("success");
+// });
 
 app.get("/", function(req, res) {
   const day = date.getDate();
